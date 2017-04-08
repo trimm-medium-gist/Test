9ea92c31552c5103ec1bf59f0cab4e969c93eadf
Oleksa Korin <oleksandr.korin@idapgroup.com>
2017-04-08 12:43:24 +0300

    project -> initial commit

---
+++ Test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Test
+//
+//  Created by Oleksa Korin on 4/8/17.
+//  Copyright © 2017 IDAP Group. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
